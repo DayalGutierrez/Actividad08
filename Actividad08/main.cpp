5d96019a4e302f19d484ee96af4480c496499414
Dayal Gutiérrez <dayal.gutierrez5863@alumnos.udg.mx>
2020-10-17 01:05:52 -0500

    Respaldar y recuperar implementado

--- Actividad08/main.cpp
+++ Actividad08/main.cpp
@@ -5,28 +5,31 @@
 using namespace std;
 
 int main(){
-    Computadora compu1 = Computadora("Dayal","Windows","Intel",8);
-    Computadora compu2;
+    Laboratorio l;
+    l.recuperar();
+    l.mostrar();
+    // Computadora compu1 = Computadora("Dayal","Windows","Intel",8);
+    // Computadora compu2;
 
-    compu2.setNombre("Gutierrez");
-    compu2.setSistemaOp("Linux");
-    compu2.setProcesador("Ryzen");
-    compu2.setMemoriaRAM(16);
+    // compu2.setNombre("Gutierrez");
+    // compu2.setSistemaOp("Linux");
+    // compu2.setProcesador("Ryzen");
+    // compu2.setMemoriaRAM(16);
 
-    Laboratorio lab;
+    // Laboratorio lab;
 
-    //lab.agregarFinal(compu1);
-    //lab.agregarFinal(compu2);
+    // //lab.agregarFinal(compu1);
+    // //lab.agregarFinal(compu2);
 
-    lab << compu1 << compu2;
+    // lab << compu1 << compu2;
 
-    Computadora compu3;
-    cin >> compu3;
-    lab << compu3;
+    // Computadora compu3;
+    // cin >> compu3;
+    // lab << compu3;
 
-    lab.mostrar();
-    lab.respaldar_tabla();
-    lab.respaldar();
+    // lab.mostrar();
+    // //lab.respaldar_tabla();
+    // //lab.respaldar();
 
     //cout << compu1;
 
